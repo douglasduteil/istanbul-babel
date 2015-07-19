49b29ea8d9226dd2c4a788f43d8ee497dfbf741f
Douglas Duteil <douglasduteil@gmail.com>
2015-07-19 21:17:11 +0200

    chore: remove comments

--- index.js
+++ index.js
@@ -6,13 +6,3 @@ var babel = require('babel-core');
 module.exports = function istanbulBabelInstrumenterTranspiler(code){
   return babel.transform(code, {sourceMaps: true});
 }
-
-/*
-var extend = require('util')._extend;
-var babel = require('babel');
-var defaultOptions = {sourceMaps: true};
-var userOptions = {stage: 0};
-var options = util._extend(defaultOptions, userOptions);
-var result = babel.transform(code, options);
-code = result.code;
- */
